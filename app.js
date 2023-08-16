f716d7e19247bfba23d87b7a61596492513c7c0e
Lovely Sharma <lovelysingeras297@gmail.com>
2023-08-17 04:08:23 +0530

    Added basic hello app

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express()
 
 require('dotenv').config()
 
+app.get('/', (req, res) => {
+    res.send("Heyy, I'm on AWS!");
+})
+
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
 })
