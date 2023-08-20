2e262ce7d8b8a8fb44b9e288d6f273a27d81590e
Lovely Sharma <lovelysingeras297@gmail.com>
2023-08-21 03:06:56 +0530

    testing again

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express')
 const app = express()
 const morgan = require("morgan")
 const ejs = require('ejs');
-
+const path = require('path')
 require('dotenv').config()
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms"))
+
 app.get('/', (req, res) => {
     res.send("Heyy, I'm on AWS!");
 })
@@ -15,10 +17,6 @@ app.get('/test', (req, res) => {
 res.render('test', {name: 'Lovely'})
 })
 
-app.delete('/pass/:id', (req, res) => {
-    console.log(req.params.id);
-    res.status(200)
-})
 
 app.listen(process.env.PORT || 3000, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
